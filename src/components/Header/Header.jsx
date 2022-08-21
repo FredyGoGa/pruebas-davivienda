c51d387d89607cc6c62cc29debec3da362e862fe
Fredy Gonzalez <fredylingling.fgg6@gmail.com>
2022-08-21 12:32:54 -0500

    creacion de componente de trabajo

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import  "./header.css";
+
+export default function Header() {
+  return (
+    <div className='header'>
+
+    <div className="iconsLeft">
+     
+    </div>
+    iconos de la parte superior izquierda
+
+    </div>
+  )
+}
---
+++ src/components/workTable/Worktable.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./worktable.css";
+
+
+
+export default function Worktable() {
+  return (
+   <section className="worktable">
+
+<h1>HOLA</h1>
+
+   </section>
+  )
+}
