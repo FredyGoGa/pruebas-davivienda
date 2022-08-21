dc7e4abbd89c4a28dd90a270c7dab8b9c01c30d5
Fredy Gonzalez <fredylingling.fgg6@gmail.com>
2022-08-21 16:38:55 -0500

    se importo png de close en el componente header, se creo un div nameUser con el nombre de prueba

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,7 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
+import close from "../../assets/img/close.png";
+import logo from "../../assets/img/logo-red.png";
 
 import  "./header.css";
 
@@ -7,13 +10,33 @@ export default function Header() {
 
   return (
     <>
+    
     <div className='sectionHeader'>
+   
+    
+    
+    <img className='logo'src={ logo }  />
+    <img className='close'src={ close }  />
+     
       
+    
+
     </div>
     
     
     
-    <div className="sectionUser">hola</div></>
+    <div className="sectionUser">
+   <div className="nameUser">
+    CRISTIAN DAVID ARIAS CORREA
+   </div>
+   
+    
+    </div>
+    
+    
+    
+    </>
+    
     
    
 
