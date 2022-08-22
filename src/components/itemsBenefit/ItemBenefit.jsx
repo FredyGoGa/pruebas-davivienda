a17a7a4306254c54774044c71bc38a5196115151
Fredy Gonzalez <fredylingling.fgg6@gmail.com>
2022-08-22 01:07:40 -0500

    imagenes de dollar incorporadas, poner el texto al lado de cada imagen pendiente

--- src/components/itemsBenefit/ItemBenefit.jsx
+++ src/components/itemsBenefit/ItemBenefit.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import "./itemBenefit.css"
 import imageBenefitPension from "../../assets/img/dolarimg.png";
-
+import imagePhoneDollar from "../../assets/img/phonedolar.png";
+import imageBalanceDollar from "../../assets/img/dolarDocument.png";
+import finalBalanceDollar from "../../assets/img/balancedolar.png";
 
 
 export default function ItemBenefit() {
@@ -14,7 +16,47 @@ export default function ItemBenefit() {
         <img className='imageBenefitPension' src={ imageBenefitPension } alt="Imagen Beneficio"/>
 
       </div>
-      <div className="Text"></div>
+
+      <div className="Text">
+
+          Reciba el pago de su pensión y otros depósitos en su cuenta
+      </div>
+
+
+      <div className="Image">
+
+<img className='imagePhoneDollar' src={ imagePhoneDollar } alt="Imagen Beneficio"/>
+
+</div>
+
+<div className="Text">
+
+  Programe de forma automática sus transferencias, el pago de productos y sevicios
+</div>
+
+
+<div className="Image">
+
+<img className='imageBalanceDollar' src={ imageBalanceDollar } alt="Imagen Beneficio"/>
+
+</div>
+
+<div className="Text">
+
+  Podrá obtener benefcios con aliados y tasas especiales en créditos para pensionados.
+</div>
+
+
+<div className="Image">
+
+<img className='finalBalanceDollar' src={ finalBalanceDollar } alt="Imagen Beneficio"/>
+
+</div>
+
+<div className="Text">
+
+  Podrá acceder a un cupo de adelanto de pensión y a los productos móviles que le ofrece Davivienda.
+</div>
 
 
 
