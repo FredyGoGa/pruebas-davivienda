cc5ec16e6ee241d34475bf2ff6fc537dfe4fbdc6
Fredy Gonzalez <fredylingling.fgg6@gmail.com>
2022-08-22 16:13:54 -0500

    primera pantalla ok

--- src/components/itemsBenefit/ItemBenefit.jsx
+++ src/components/itemsBenefit/ItemBenefit.jsx
@@ -8,63 +8,48 @@ import finalBalanceDollar from "../../assets/img/balancedolar.png";
 
 export default function ItemBenefit() {
   return (
-    <div className="ItemBenefit">
+<>
+    <div className="containerTable" >
+      <table className="demo" >
+        <tbody >
+        <tr >
+          <td width="10%"><img className='imageBenefitPension' src={ imageBenefitPension } alt="Imagen Beneficio"/></td>
+          <td ><p className="Text" valing="top">Reciba  el pago de su pensionados
+              y otros depositos de su cuenta.</p></td>
+        </tr>
+        <tr>
+          <td width="10%" ><img className='imagePhoneDollar' src={ imagePhoneDollar } alt="Imagen Beneficio"/></td>
+          <td> <p className="Text">Programe de forma automática sus transferencias, el pago de productos y servicios.</p></td>
+        </tr>
+        <tr>
+          <td width="10%" ><img className='imageBalanceDollar' src={ imageBalanceDollar } alt="Imagen Beneficio"/></td>
+          <td >  <p className="Text">Podrá obtener benefcios con aliados y tasas especiales en créditos para pensionados.</p></td>
+        </tr>
+        <tr>
+          <td width="10%" ><img className='finalBalanceDollar' src={ finalBalanceDollar } alt="Imagen Beneficio"/></td>
+          <td > <p className="Text">Podrá acceder a un cupo de adelanto de pensión y a los productos móviles que le ofrece Davivienda.</p></td>
+        </tr>
+        </tbody>
+      </table>
 
 
-      <div className="Image">
-
-        <img className='imageBenefitPension' src={ imageBenefitPension } alt="Imagen Beneficio"/>
-
+      <center><div className='footer'  >
+          <p>Este producto cuenta con el seguro de depósitos <strong>FOGAFIN</strong></p>
       </div>
+      </center>
 
-      <div className="Text">
-
-          Reciba el pago de su pensión y otros depósitos en su cuenta
-      </div>
-
-
-      <div className="Image">
-
-<img className='imagePhoneDollar' src={ imagePhoneDollar } alt="Imagen Beneficio"/>
-
-</div>
-
-<div className="Text">
-
-  Programe de forma automática sus transferencias, el pago de productos y sevicios
-</div>
-
-
-<div className="Image">
-
-<img className='imageBalanceDollar' src={ imageBalanceDollar } alt="Imagen Beneficio"/>
-
-</div>
-
-<div className="Text">
-
-  Podrá obtener benefcios con aliados y tasas especiales en créditos para pensionados.
-</div>
-
-
-<div className="Image">
-
-<img className='finalBalanceDollar' src={ finalBalanceDollar } alt="Imagen Beneficio"/>
-
-</div>
-
-<div className="Text">
-
-  Podrá acceder a un cupo de adelanto de pensión y a los productos móviles que le ofrece Davivienda.
-</div>
-
-
-
-
-
-
-
-
+      <center><button> Continuar
+      </button></center>
+      
     </div>
+
+    
+
+
+
+
+</>
+
+    
   )
 }
