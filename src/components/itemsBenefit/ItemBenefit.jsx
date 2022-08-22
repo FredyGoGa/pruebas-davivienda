c6c8e6c969a7d707850e123bef5de73ea25569ff
Fredy Gonzalez <fredylingling.fgg6@gmail.com>
2022-08-21 23:50:25 -0500

    imagenes con estilos

---
+++ src/components/itemsBenefit/ItemBenefit.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./itemBenefit.css"
+import imageBenefitPension from "../../assets/img/dolarimg.png";
+
+
+
+export default function ItemBenefit() {
+  return (
+    <div className="ItemBenefit">
+
+
+      <div className="Image">
+
+        <img className='imageBenefitPension' src={ imageBenefitPension } alt="Imagen Beneficio"/>
+
+      </div>
+      <div className="Text"></div>
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
--- src/components/workTable/Worktable.jsx
+++ src/components/workTable/Worktable.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from "../Header/Header";
+import ItemBenefit from "../itemsBenefit/ItemBenefit";
 
 import "./worktable.css";
 
@@ -11,7 +12,7 @@ export default function Worktable() {
    <section className="worktable">
 
    <Header userName= "FREDY GONZALEZ GARCIA" title="Prueba" description="FormulariO" />
-
+   <ItemBenefit/>
 
    </section>
   )
