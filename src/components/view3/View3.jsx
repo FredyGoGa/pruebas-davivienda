fdc477f09e0e47c711d5e15e16d13466cfc6b884
Fredy Gonzalez <fredylingling.fgg6@gmail.com>
2022-08-23 16:28:04 -0500

    Presenta screens en read.md

--- src/components/view3/View3.jsx
+++ src/components/view3/View3.jsx
@@ -8,7 +8,7 @@ import "./view3.css";
 
 export default function View3() {
   return (
-  <section className="view2">
+  <section className="view3">
     <Header userName= "FREDY GONZALEZ GARCIA" title="Compra de Cartera Móvil" description="Resultado" />
     <center>
       <div className="containerGeneral">
@@ -18,35 +18,36 @@ export default function View3() {
           </tr>
           <tr>
             <td width=""><img className='close' src={ iconogris } alt="iconogris"/></td>
-            <td className="textIconTable">Los productos fuerosn aprobados 
+            <td className="textPlazo">Los productos fueron aprobados
             satisfactoriamente.</td>
           </tr>
           <tr>
-            <td colspan="2"><p>En un plazo máximo de <strong>5 días calendario</strong>, se le informará por medio de un mensaje de texto el estado de sus productos.</p></td>
+            <td colspan="2">
+              <p className="textPlazoDetails">En un plazo máximo de <strong>5 días calendario</strong>, se le informará por medio de un mensaje de texto el estado de sus productos.</p></td>
           </tr>
           <tr>
             <td className="textIconTable" colspan="2">
                 <p className="textResultName">Adelanto de pensión:</p>
-                <p className="textResultDetails">Aprobado</p>  
+                <p className="textResultDetails">Aprobado</p>
             </td>
           </tr>
           <tr>
             <td className="textIconTable" colspan="2">
               <p className="textResultName">Tipo de seguro:</p>
-              <p className="textResultDetails">Familiar- 106.200 mensual</p> 
+              <p className="textResultDetails">Familiar- 106.200 mensual</p>
             </td>
           </tr>
           <tr>
             <td className="textIconTable" colspan="2">
               <p className="textResultName">Fecha y hora:</p>
-              <p className="textResultDetails">29/01/2020, 09:41 am</p> 
+              <p className="textResultDetails">29/01/2020, 09:41 am</p>
             </td>
           </tr>
         </table>
       </div>
-    
+
     <br></br>
-    
+
     <button className="continueButton"> Finalizar
       </button></center>
   </section>
